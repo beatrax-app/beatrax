7944c3ddfd7bcecc82880f0d9685d53fd0888a8f
Wessel Verheij <info@nightworks.io>
2026-05-25 18:25:46 +0200

    fix(16.1-review): CR-01 mount SuggestMappingModal globally so triage CTA opens it

--- Modules/Community/Resources/views/mystery-merchants.blade.php
+++ Modules/Community/Resources/views/mystery-merchants.blade.php
@@ -4,7 +4,6 @@
     <main class="min-h-screen bg-white dark:bg-slate-950">
         <div class="mx-auto max-w-5xl px-8 py-12">
             @livewire('community.mystery-merchants-page')
-            @livewire('community.suggest-mapping-modal')
         </div>
     </main>
 @endsection
